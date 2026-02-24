0a19ab34c79618997b293b7ef6f3b4e388cf7e57
Victor Gabriel Costin <vcostin@drivenets.com>
2026-02-24 19:15:46 +0000

    do only one iteration so that syscalls don't get measured
    Signed-off-by: Victor Gabriel Costin <vcostin@drivenets.com>

--- uds_vs_mutex.cpp
+++ uds_vs_mutex.cpp
@@ -7,11 +7,13 @@
 #include <sbench.hpp>
 
 #ifdef DEBUG
-    constexpr int NUM_MESSAGES = 5;
+    constexpr int NUM_MESSAGES = 3;
+    constexpr int NUM_ITERATIONS = 2;
     #define DEBUG_PRINT(x) std::cout << x << std::endl;
     #define DEBUG_BLOCK(x) do { x; } while (0)
 #else
-    constexpr int NUM_MESSAGES = 100000;
+    constexpr int NUM_MESSAGES = 1000000;
+    constexpr int NUM_ITERATIONS = 15;
     #define DEBUG_PRINT(x)
     #define DEBUG_BLOCK(x) do { } while (0)
 #endif
@@ -27,30 +29,30 @@ void test_thread_safe_queue(sbench::SBench& bench)
     DEBUG_PRINT("test_thread_safe_queue");
     ThreadSafeQueue thread_safe_queue;
 
+    std::thread producer([&thread_safe_queue]() {
+        for (int i = 0; i < NUM_MESSAGES; i++) {
+            thread_safe_queue.addMessage("Hello, world! " + std::to_string(i));
+        }
+    });
+
+    int processed_messages = 0;
+    std::thread consumer([&thread_safe_queue, &processed_messages]() {
+        while (processed_messages != NUM_MESSAGES) {
+            thread_safe_queue.waitMessages();
+            size_t num_messages = thread_safe_queue.processMessages([](std::string& message) {
+                workload(message);
+            });
+
+            processed_messages += num_messages;
+
+            DEBUG_BLOCK(
+                if (num_messages != 0)
+                    std::cout << "Processed " << num_messages << " messages\n";
+            );
+        }
+    });
+
     bench.run("thread_safe_message_queue", [&]() {
-        std::thread producer([&thread_safe_queue]() {
-            for (int i = 0; i < NUM_MESSAGES; i++) {
-                thread_safe_queue.addMessage("Hello, world! " + std::to_string(i));
-            }
-        });
-        
-        int processed_messages = 0;
-        std::thread consumer([&thread_safe_queue, &processed_messages]() {
-            while (processed_messages != NUM_MESSAGES) {
-                thread_safe_queue.waitMessages();
-                size_t num_messages = thread_safe_queue.processMessages([](std::string& message) {
-                    workload(message);
-                });
-
-                processed_messages += num_messages;
-
-                DEBUG_BLOCK(
-                    if (num_messages != 0)
-                        std::cout << "Processed " << num_messages << " messages\n";
-                );
-            }
-        });
-
         producer.join();
         consumer.join();
     });
@@ -64,30 +66,30 @@ void test_thread_safe_queue_optimized(sbench::SBench& bench)
     DEBUG_PRINT("test_thread_safe_queue_optimized");
     ThreadSafeQueueOptimized thread_safe_queue_optimized;
 
+    std::thread producer([&thread_safe_queue_optimized]() {
+        for (int i = 0; i < NUM_MESSAGES; i++) {
+            thread_safe_queue_optimized.addMessage("Hello, world! " + std::to_string(i));
+        }
+    });
+
+    int processed_messages = 0;
+    std::thread consumer([&thread_safe_queue_optimized, &processed_messages]() {
+        while (processed_messages != NUM_MESSAGES) {
+            thread_safe_queue_optimized.waitMessages();
+            size_t num_messages = thread_safe_queue_optimized.processMessages([](std::string& message) {
+                workload(message);
+            });
+
+            processed_messages += num_messages;
+
+            DEBUG_BLOCK(
+                if (num_messages != 0)
+                    std::cout << "Processed " << num_messages << " messages\n";
+            );
+        }
+    });
+
     bench.run("test_thread_safe_queue_optimized", [&]() {
-        std::thread producer([&thread_safe_queue_optimized]() {
-            for (int i = 0; i < NUM_MESSAGES; i++) {
-                thread_safe_queue_optimized.addMessage("Hello, world! " + std::to_string(i));
-            }
-        });
-
-        int processed_messages = 0;
-        std::thread consumer([&thread_safe_queue_optimized, &processed_messages]() {
-            while (processed_messages != NUM_MESSAGES) {
-                thread_safe_queue_optimized.waitMessages();
-                size_t num_messages = thread_safe_queue_optimized.processMessages([](std::string& message) {
-                    workload(message);
-                });
-
-                processed_messages += num_messages;
-
-                DEBUG_BLOCK(
-                    if (num_messages != 0)
-                        std::cout << "Processed " << num_messages << " messages\n";
-                );
-            }
-        });
-
         producer.join();
         consumer.join();
     });
@@ -101,23 +103,23 @@ void test_socket_queue(sbench::SBench& bench)
     DEBUG_PRINT("test_socket_queue");
     SocketQueue socket_queue("/tmp/mvi_socket_queue");
 
+    std::thread producer([&socket_queue]() {
+        socket_queue.waitForConsumer();
+        for (int i = 0; i < NUM_MESSAGES; i++) {
+            socket_queue.sendMessage("Hello, world! " + std::to_string(i));
+        }
+    });
+
+    std::thread consumer([&socket_queue]() {
+        socket_queue.connectToProducer();
+        for (int i = 0; i < NUM_MESSAGES; i++) {
+            socket_queue.receiveMessage([](std::string& message) {
+                workload(message);
+            });
+        }
+    });
+
     bench.run("socket_queue", [&]() {
-        std::thread producer([&socket_queue]() {
-            socket_queue.waitForConsumer();
-            for (int i = 0; i < NUM_MESSAGES; i++) {
-                socket_queue.sendMessage("Hello, world! " + std::to_string(i));
-            }
-        });
-
-        std::thread consumer([&socket_queue]() {
-            socket_queue.connectToProducer();
-            for (int i = 0; i < NUM_MESSAGES; i++) {
-                socket_queue.receiveMessage([](std::string& message) {
-                    workload(message);
-                });
-            }
-        });
-
         DEBUG_PRINT("Waiting for threads to join");
         producer.join();
         consumer.join();
@@ -130,7 +132,6 @@ void test_socket_queue(sbench::SBench& bench)
 int main()
 {
     sbench::SBench bench("mvi_cpp");
-    bench.iteration(15);
 
     test_socket_queue(bench);
     test_thread_safe_queue(bench);
