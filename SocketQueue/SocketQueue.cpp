1753ac43481ff056102ebadf7ece5f4b50c863ec
Victor Gabriel Costin <vcostin@drivenets.com>
2026-02-25 08:34:36 +0000

    add SocketQueueStream to directly stream info across the socket
    Signed-off-by: Victor Gabriel Costin <vcostin@drivenets.com>

--- SocketQueue/SocketQueue.cpp
+++ SocketQueue/SocketQueue.cpp
@@ -11,7 +11,9 @@
 #include <string.h>
 #include <cassert>
 
-SocketQueue::SocketQueue(const std::string path)
+#define STRICT_MESSAGE_SIZE 13 // "Hello, world!"
+
+SocketQueuePacket::SocketQueuePacket(const std::string path)
 {
     auto ret = socket(AF_UNIX, SOCK_SEQPACKET, 0);
     mListenFd = ret;
@@ -27,7 +29,7 @@ SocketQueue::SocketQueue(const std::string path)
     mSocketPath = path;
 }
 
-SocketQueue::~SocketQueue()
+SocketQueuePacket::~SocketQueuePacket()
 {
     if (mListenFd >= 0) {
         close(mListenFd);
@@ -43,7 +45,7 @@ SocketQueue::~SocketQueue()
     }
 }
 
-void SocketQueue::waitForConsumer()
+void SocketQueuePacket::waitForConsumer()
 {
 #ifdef DEBUG
     std::cout << "Waiting for consumer" << std::endl;
@@ -61,7 +63,7 @@ void SocketQueue::waitForConsumer()
 #endif
 }
 
-void SocketQueue::connectToProducer()
+void SocketQueuePacket::connectToProducer()
 {
 #ifdef DEBUG
     std::cout << "Connecting to producer" << std::endl;
@@ -84,7 +86,7 @@ void SocketQueue::connectToProducer()
 #endif
 }
 
-void SocketQueue::sendMessage(const std::string& message)
+void SocketQueuePacket::sendMessage(const std::string& message)
 {
     #define MESSAGE_SIZE 1024
     char buf[MESSAGE_SIZE];
@@ -109,7 +111,7 @@ void SocketQueue::sendMessage(const std::string& message)
     assert(n == message.size());
 }
 
-void SocketQueue::receiveMessage(std::function<void(std::string&)> consumeCb)
+void SocketQueuePacket::receiveMessage(std::function<void(std::string&)> consumeCb)
 {
     #define MESSAGE_SIZE 1024
     char buf[MESSAGE_SIZE];
@@ -146,7 +148,172 @@ void SocketQueue::receiveMessage(std::function<void(std::string&)> consumeCb)
     consumeCb(message);
 }
 
-void SocketQueue::cleanup()
+void SocketQueuePacket::cleanup()
+{
+    if (mConsumerFd >= 0) {
+        close(mConsumerFd);
+        mConsumerFd = -1;
+    }
+    if (mProducerFd >= 0) {
+        close(mProducerFd);
+        mProducerFd = -1;
+    }
+}
+
+SocketQueueStream::SocketQueueStream(const std::string path)
+{
+    auto ret = socket(AF_UNIX, SOCK_STREAM, 0);
+    mListenFd = ret;
+
+    unlink(path.c_str());
+    struct sockaddr_un addr = {};
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path.c_str(), sizeof(addr.sun_path) - 1);
+    ret = bind(mListenFd, reinterpret_cast<struct sockaddr*>(&addr), sizeof(addr));
+    assert(ret == 0);
+    ret = listen(mListenFd, 1);
+    assert(ret == 0);
+    mSocketPath = path;
+}
+
+SocketQueueStream::~SocketQueueStream()
+{
+    if (mListenFd >= 0) {
+        close(mListenFd);
+        mListenFd = -1;
+    }
+    if (mConsumerFd >= 0) {
+        close(mConsumerFd);
+        mConsumerFd = -1;
+    }
+    if (mProducerFd >= 0) {
+        close(mProducerFd);
+        mProducerFd = -1;
+    }
+}
+
+void SocketQueueStream::waitForConsumer()
+{
+#ifdef DEBUG
+    std::cout << "Waiting for consumer" << std::endl;
+#endif
+    if (mConsumerFd >= 0) {
+        close(mConsumerFd);
+        mConsumerFd = -1;
+    }
+
+    auto ret = accept(mListenFd, nullptr, nullptr);
+    assert(ret >= 0);
+    mConsumerFd = ret;
+#ifdef DEBUG
+    std::cout << "Consumer connected" << std::endl;
+#endif
+}
+
+void SocketQueueStream::connectToProducer()
+{
+#ifdef DEBUG
+    std::cout << "Connecting to producer" << std::endl;
+#endif
+    if (mProducerFd >= 0) {
+        close(mProducerFd);
+        mProducerFd = -1;
+    }
+
+    auto ret = socket(AF_UNIX, SOCK_STREAM, 0);
+    mProducerFd = ret;
+
+    struct sockaddr_un addr = {};
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, mSocketPath.c_str(), sizeof(addr.sun_path) - 1);
+    ret = connect(mProducerFd, reinterpret_cast<struct sockaddr*>(&addr), sizeof(addr));
+    assert(ret == 0);
+#ifdef DEBUG
+    std::cout << "Connected to producer" << std::endl;
+#endif
+}
+
+void SocketQueueStream::sendMessage(const std::string& message)
+{
+    ssize_t n = write(mConsumerFd, message.c_str(), message.size());
+    if (n < 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            return;
+        }
+#ifdef DEBUG
+        std::cout << "sendmsg failed: " << strerror(errno) << std::endl;
+#endif
+        return;
+    }
+    assert(n == message.size());
+}
+
+void SocketQueueStream::receiveMessage(std::function<void(std::string&)> consumeCb)
+{
+    #define MESSAGE_SIZE 1024
+    char buf[MESSAGE_SIZE];
+
+    ssize_t n = read(mProducerFd, buf, STRICT_MESSAGE_SIZE);
+    if (n < 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            return;
+        }
+#ifdef DEBUG
+        std::cout << "recvmsg failed: " << strerror(errno) << std::endl;
+#endif
+        return;
+    }
+
+    if (n == 0) {
+        return;
+    }
+
+    assert(n == STRICT_MESSAGE_SIZE);
+
+    buf[n] = '\0';
+    std::string message(buf, n);
+    consumeCb(message);
+}
+
+void SocketQueueStream::sendPointer(void* pointer)
+{
+    ssize_t n = write(mConsumerFd, &pointer, sizeof(pointer));
+    if (n < 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            return;
+        }
+#ifdef DEBUG
+        std::cout << "sendmsg failed: " << strerror(errno) << std::endl;
+#endif
+        return;
+    }
+    assert(n == sizeof(pointer));
+}
+
+void SocketQueueStream::receivePointer(std::function<void(void*)> consumeCb)
+{
+    void* pointer = nullptr;
+
+    ssize_t n = read(mProducerFd, &pointer, sizeof(pointer));
+    if (n < 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            return;
+        }
+#ifdef DEBUG
+        std::cout << "recvmsg failed: " << strerror(errno) << std::endl;
+#endif
+        return;
+    }
+
+    if (n == 0) {
+        return;
+    }
+
+    assert(n == sizeof(pointer));
+    consumeCb(pointer);
+}
+
+void SocketQueueStream::cleanup()
 {
     if (mConsumerFd >= 0) {
         close(mConsumerFd);
--- SocketQueue/SocketQueue.hpp
+++ SocketQueue/SocketQueue.hpp
@@ -3,10 +3,10 @@
 #include <string>
 #include <functional>
 
-class SocketQueue {
+class SocketQueuePacket {
 public:
-    SocketQueue(const std::string path);
-    ~SocketQueue();
+    SocketQueuePacket(const std::string path);
+    ~SocketQueuePacket();
 
     void waitForConsumer();
     void connectToProducer();
@@ -16,6 +16,33 @@ public:
 
     // method used only by the consumer thread
     void receiveMessage(std::function<void(std::string&)> consumeCb);
+
+    void cleanup();
+
+private:
+    int mListenFd = -1;
+    int mConsumerFd = -1;
+    int mProducerFd = -1;
+
+    std::string mSocketPath;
+};
+
+class SocketQueueStream {
+public:
+    SocketQueueStream(const std::string path);
+    ~SocketQueueStream();
+
+    void waitForConsumer();
+    void connectToProducer();
+
+    // method used only by the producer thread
+    void sendMessage(const std::string& message);
+
+    // method used only by the consumer thread
+    void receiveMessage(std::function<void(std::string&)> consumeCb);
+
+    void sendPointer(void* pointer);
+    void receivePointer(std::function<void(void*)> consumeCb);
 
     void cleanup();
 
